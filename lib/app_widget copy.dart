aa67319208e1a664a781c3ad5bafdd38cb535a2f
Eliana França <28299486+elianamsf@users.noreply.github.com>
2023-06-24 20:07:07 -0300

    commit inicial

---
+++ lib/app_widget copy.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import 'home_page.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        brightness: Brightness.light,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
---
+++ lib/home_page.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+import 'app_controller.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() {
+    return HomePageState();
+  }
+}
+
+class HomePageState extends State<HomePage> {
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Calc Soft"),
+        iconTheme: const IconThemeData(),
+        actions: [
+          CustomSwitch(),
+        ],
+      ),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(children: [
+          Text('Contador: $counter'),
+          CustomSwitch(),
+        ]),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          setState(() {
+            counter++;
+          });
+        },
+      ),
+    );
+  }
+}
+
+class CustomSwitch extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Switch(
+      value: AppController.instance.isDartTheme,
+      onChanged: (value) {
+        AppController.instance.changeTheme();
+      },
+    );
+  }
+}
