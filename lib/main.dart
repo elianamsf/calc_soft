192e24311454bebf9dec5274449c47ef606824ce
Eliana França <28299486+elianamsf@users.noreply.github.com>
2023-06-25 23:26:58 -0300

    init firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import 'app_widget.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 main() {
   runApp(AppWidget());
